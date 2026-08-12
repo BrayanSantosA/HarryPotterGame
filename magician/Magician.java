e5500c78b519a270ee77b56a0d24954e65abec78
Brayan Santos <bdsantosa@udistrital.edu.co>
2026-08-12 09:45:52 -0500

    feat: Initial magician class

---
+++ magician/Magician.java
@@ -0,0 +1,13 @@
+package magician;
+import SpellBehaviour, SpellBehaviour;
+
+public abstract class Magician {
+
+    SpellBehaviour, SpellBehaviour;
+
+    public void performSpell(){
+
+        spellbehaviour.spell();
+    }
+    
+}
